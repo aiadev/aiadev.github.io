12817161f60ed2bd9532877e6e0ae48775040d62
aleia <13177558+aiadev@users.noreply.github.com>
2020-08-08 17:22:09 -0300

    Add files via upload

--- drawPad/index.js
+++ drawPad/index.js
@@ -32,9 +32,62 @@ var svg = d3.select("#svgContainer").append("svg")
 	.attr("width", "100%")
 	.attr("height","100%")
   	.call(d3.drag()
-		  .on("start", startDrawing)
-		  .on("drag", draw)
-		  .on("end", endDrawing));
+		  .on("start", startDraw)
+		  .on("drag", dragDraw)
+		  .on("end", endDraw))
+	.on("click", clicked); // dragged
+
+// CLICK
+function clicked(d, i) { 
+	// diferencia un click de un drag
+//	if (d3.event.defaultPrevented) return; 
+}
+
+// DRAG START
+function startDraw(d,t) {
+//    d3.select(this).attr("stroke", "black");
+	startPos[0] = d3.event.x;
+	startPos[1] = d3.event.y;	
+	endPos[0] = d3.event.x;
+	endPos[1] = d3.event.y;
+
+	console.log(".. DRAWING " + buttonData[modeSel].id)
+	console.log("   Start position: " + startPos);
+
+	prevLine.attr("visibility", modeSel == 0 ? "visible" : "hidden");
+	prevRect.attr("visibility", modeSel == 1 ? "visible" : "hidden");
+}
+
+// DRAGGING
+function dragDraw(d,t) {
+	endPos[0] = d3.event.x;
+	endPos[1] = d3.event.y;
+	if (modeSel == 0) {
+		prevLine
+			.attr("x1", startPos[0])
+			.attr("y1", startPos[1])
+			.attr("x2", endPos[0])
+			.attr("y2", endPos[1]);		
+	} else {
+		prevRect 
+			.attr("x", d3.min([startPos[0], endPos[0]]))
+			.attr("y", d3.min([startPos[1], endPos[1]]))
+			.attr("width", Math.abs(startPos[0]-endPos[0])) 
+			.attr("height", Math.abs(startPos[1]-endPos[1]));		
+	}
+}
+
+// DRAG END
+function endDraw(d,t) {
+	endPos[0] = d3.event.x;
+	endPos[1] = d3.event.y;
+	console.log("   End position: "+ endPos);
+
+	prevLine.attr("visibility", "hidden");
+	prevRect.attr("visibility", "hidden");
+
+	(modeSel == 0) ? drawNewLine() : drawNewRect();
+}
 
 var canvasGroup = svg.append("g").attr("id", "canvasGroup");
 
@@ -58,26 +111,6 @@ var prevRect = prevGroup.append("rect")
 	.classed("prevForm", true)
 	.attr("fill", "transparent")
 	.attr("visibility", "hidden");
-
-// previsualizacion de la forma segun el modo seleccionado
-function draw() {	
-	endPos[0] = d3.event.x;
-	endPos[1] = d3.event.y;
-	
-	if (modeSel == 0) {
-		prevLine
-			.attr("x1", startPos[0])
-			.attr("y1", startPos[1])
-			.attr("x2", endPos[0])
-			.attr("y2", endPos[1]);		
-	} else {
-		prevRect 
-			.attr("x", d3.min([startPos[0], endPos[0]]))
-			.attr("y", d3.min([startPos[1], endPos[1]]))
-			.attr("width", Math.abs(startPos[0]-endPos[0])) 
-			.attr("height", Math.abs(startPos[1]-endPos[1]));		
-	}
-}
 
 // setup herramientas/modos del menu
 var buttonGroup = d3.select("#menuContainer").append("g")
@@ -112,32 +145,6 @@ function selectTool (d,i){
 function clearCanvas() {
     lineGroup.selectAll("line").remove();
     rectGroup.selectAll("rect").remove();
-}
-
-// setea el punto de inicio de la forma segun el modo seleccionado
-function startDrawing() {
-	startPos[0] = d3.event.x;
-	startPos[1] = d3.event.y;	
-	endPos[0] = d3.event.x;
-	endPos[1] = d3.event.y;
-
-    console.log(".. DRAWING " + buttonData[modeSel].id)
-	console.log("   Start position: " + startPos);
-	
-	prevLine.attr("visibility", modeSel == 0 ? "visible" : "hidden");
-	prevRect.attr("visibility", modeSel == 1 ? "visible" : "hidden");
-}
-
-// finaliza el dibujo de la forma
-function endDrawing() {
-	endPos[0] = d3.event.x;
-	endPos[1] = d3.event.y;
-    console.log("   End position: "+ endPos);
-    
-	prevLine.attr("visibility", "hidden");
-	prevRect.attr("visibility", "hidden");
-
-	(modeSel == 0) ? drawNewLine() : drawNewRect();
 }
 
 // dibuja una linea en el canvas
