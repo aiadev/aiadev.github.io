e3d5b944895ac57281d81ef2417fc28e03f7f7ea
aleia <13177558+aiadev@users.noreply.github.com>
2021-04-23 18:34:45 -0300

    Add files via upload
    con size y pitch

--- P5js/test_01/sketch.js
+++ P5js/test_01/sketch.js
@@ -1,24 +1,35 @@
 let size = 50;
 let sound;
 let alpha = 1;
+let pitch = 1;
+
+function preload() {
+  sound = loadSound('assets/sound/sound.wav');
+}
 
 function setup() {
-  sound = loadSound('assets/sound/sound.wav');
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
+  updatePitch();
   background(255);
   fill(255);
-  circle(mouseX,mouseY,size);
+  circle(mouseX,mouseY,size+10);
   if (sound.isLoaded() && sound.isPlaying()) {
     alpha = sound.currentTime()/sound.duration();
     fill(0,255-255*alpha);
-    circle(mouseX,mouseY,size-1);
   }
+  circle(mouseX,mouseY,size);
 }
 
-function mouseClicked() {
-  size*=1.1;
+function mouseClicked() { 
   if (sound.isLoaded()) sound.play();
+}
+
+function updatePitch() {
+  pitch = map(mouseX, 0.1, width, 0.5, 4);
+  pitch = constrain(pitch, 0.1, 4);
+  sound.rate(pitch);
+  size = 200-pitch*45;
 }
