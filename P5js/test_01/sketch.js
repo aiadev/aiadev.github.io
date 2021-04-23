e727238f63e5e7ae50e14815bd1db4a0f82662ff
aleia <13177558+aiadev@users.noreply.github.com>
2021-04-23 17:26:48 -0300

    Add files via upload
    con sonido sketch

--- P5js/test_01/sketch.js
+++ P5js/test_01/sketch.js
@@ -1,15 +1,24 @@
 let size = 50;
+let sound;
+let inColor;
 
 function setup() {
+  sound = loadSound('assets/sound/sound.wav');
   createCanvas(400, 400);
+  inColor = color(255,0,0);
 }
 
 function draw() {
   background(220);
-  fill(255);
+  if (sound.isPlaying()) {
+    fill(0,255,0);
+  } else {
+    fill(255,0,0);
+  }
   circle(mouseX,mouseY,size);
 }
 
 function mousePressed() {
   size*=1.1;
+  sound.play();
 }
